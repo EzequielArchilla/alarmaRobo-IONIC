afc3d0a47d4586d13cb28bc507325597bafda6c1
EzequielArchilla <ezequiel.a.archilla@gmail.com>
2022-09-19 16:13:19 -0300

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,40 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'splash',
+    pathMatch: 'full',
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'home',
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomePageModule),
+  },
+  {
+    path: 'registro',
+    loadChildren: () =>
+      import('./registro/registro.module').then((m) => m.RegistroPageModule),
+  },
+  {
+    path: 'ingreso',
+    loadChildren: () =>
+      import('./ingreso/ingreso.module').then((m) => m.IngresoPageModule),
+  },
+  {
+    path: 'main',
+    loadChildren: () =>
+      import('./main/main.module').then((m) => m.MainPageModule),
+  },
+  {
+    path: 'splash',
+    loadChildren: () =>
+      import('./splash/splash.module').then((m) => m.SplashPageModule),
   },
 ];
-
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  spinnerHidden: boolean;
 
-  constructor() {}
+  constructor(private router: Router, public loadingCtrl: LoadingController) {
+    this.spinnerHidden = false;
+  }
 
+  ngOnInit() {}
+
+  navigateTo(url: string) {
+    this.autoHideShow();
+    setTimeout(() => {
+      this.router.navigateByUrl(url);
+    }, 2000);
+  }
+
+  autoHideShow() {
+    this.loadingCtrl
+      .create({
+        message: 'Cargando...',
+        duration: 2000,
+        translucent: true,
+        cssClass: 'ion-loading-class',
+        spinner: 'crescent',
+      })
+      .then((res) => {
+        res.present();
+        res.onDidDismiss().then((res2) => {
+          console.log('Loader closed', res2);
+        });
+      });
+  }
 }
