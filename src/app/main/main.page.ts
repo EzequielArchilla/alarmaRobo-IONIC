60ee6e7a2dfda0bee4decc57e2070bac8660bb4f
Ezequiel Archilla <earchilla@cefas.com.ar>
2022-09-23 12:15:48 -0300

    cambios

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Gyroscope, GyroscopeOrientation, GyroscopeOptions } from '@ionic-native/gyroscope/ngx';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPage implements OnInit {
 
-  constructor() { }
-
   ngOnInit() {
   }
+
+
+  constructor(private gyroscope: Gyroscope) { }
+
+  activarAlarma()
+  {
+
+  let options: GyroscopeOptions = {
+    frequency: 1000
+  };
+
+  this.gyroscope.getCurrent(options)
+    .then((orientation: GyroscopeOrientation) => {
+      console.log(orientation.x, orientation.y, orientation.z, orientation.timestamp);
+    })
+    .catch()
+
+
+  this.gyroscope.watch()
+    .subscribe((orientation: GyroscopeOrientation) => {
+        console.log(orientation.x, orientation.y, orientation.z, orientation.timestamp);
+    });
+    }
 
 }
